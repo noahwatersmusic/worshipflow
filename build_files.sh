c5bda7990ba4e8ad235cdc3ec78a38df191d40c2
Noah Waters <noahwatersmusic@gmail.com>
2026-02-15 13:25:13 -0600

    Fix build script for Vercel's uv-managed Python environment

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --no-input
 python manage.py migrate
